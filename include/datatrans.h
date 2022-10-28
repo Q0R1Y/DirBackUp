14296eb44ef0d47a439e17c6b74b8e882af402aa
z1j5y <z1j5y9939@gmail.com>
2022-10-28 23:18:21 +0800

    modified datatrans.*

--- include/datatrans.h
+++ include/datatrans.h
@@ -7,8 +7,8 @@
 
 struct Data_trans
 {
-	std::string src;
-	std::string des;
+	char src[100];
+	char des[100];
 	int begin;
 	int size;
 };
@@ -18,14 +18,14 @@ void *data_trans_thread(void *args);
 
 // copy form file "src" to file "des" from "begin" with bolck of "size"
 // return 0 if success
-int data_from_to(const std::string &src,const std::string &des,int begin,int size);
+int data_from_to(std::string src, std::string des, int begin, int size);
 
 // 0 for success
-int del_file(const std::string &src);
+int del_file(std::string src);
 
 // 1 for exist
-int file_exist(const std::string &src,const std::string &dir);
+int file_exist(std::string src,std::string dir);
 
 // 0 for success
-int cp_file(const std::string &src,const std::string &des);
+int cp_file(std::string src,std::string des);
 #endif
--- src/datatrans.cpp
+++ src/datatrans.cpp
@@ -1,72 +1,73 @@
-#include<datatrans.h>
-#include<iostream>
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<dirent.h>
-#include<stdlib.h>
-#include<string.h>
-#include<string>
-#include<vector>
+#include <datatrans.h>
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <vector>
 
 extern std::string SRC_DIR;
 extern std::string BACKUP_DIR;
 
-void *data_trans_thread(void *args)
+int data_from_to(std::string src, std::string des, int begin, int size)
 {
-	void *re=malloc(sizeof(int));
-	*(int*)re=data_from_to(((Data_trans*)args)->src.c_str(),((Data_trans*)args)->des.c_str(),
-				((Data_trans*)args)->begin,((Data_trans*)args)->size);
-	return re;
-}
-
-int data_from_to(const std::string &src,const std::string &des,int begin,int size)
-{
-	int fds,fdd;
+	int fds, fdd;
 	umask(0);
-	if((fds=open(src.c_str(),O_RDONLY))==-1||(fdd=open(des.c_str(),O_WRONLY|O_CREAT,0644))==-1)
-		return 1;
+	printf("%s %s %d\n", src.c_str(), des.c_str(), begin);
+	if ((fds = open(src.c_str(), O_RDONLY)) == -1 || (fdd = open(des.c_str(), O_WRONLY | O_CREAT, 0644)) == -1)
+		return perror("32 open() error"), 1;
 
-	if(lseek(fds,begin,SEEK_SET)==-1||lseek(fdd,begin,SEEK_SET)==-1)
-		return 1;
-	
+	if (lseek(fds, begin, SEEK_SET) == -1 || lseek(fdd, begin, SEEK_SET) == -1)
+		return perror("35 lseek() error"), 1;
+
 	int len;
 	char buf[1007];
-	for(int i=0;i<size/sizeof(buf)+1;++i)
+	for (int i = 0; i < size / sizeof(buf) + 1; ++i)
 	{
-		if((len=read(fds,buf,sizeof(buf)))==-1)
+		if ((len = read(fds, buf, sizeof(buf))) == -1)
 			return 1;
-		if(write(fdd,buf,len)==-1)
+		if (write(fdd, buf, len) == -1)
 			return 1;
 	}
 
 	close(fds);
 	close(fdd);
 
-
 	return 0;
 }
 
-int del_file(const std::string &src)
+void *data_trans_thread(void *args)
 {
-	DIR* path=opendir(src.c_str());
-	if(path==NULL)
+	void *re = malloc(sizeof(int));
+	*(int *)re = data_from_to(((Data_trans *)args)->src, ((Data_trans *)args)->des,
+							  ((Data_trans *)args)->begin, ((Data_trans *)args)->size);
+	printf("%d re:%d\n", ((Data_trans *)args)->begin, *(int *)re);
+	return re;
+}
+
+int del_file(std::string src)
+{
+	DIR *path = opendir(src.c_str());
+	if (path == NULL)
 		return remove(src.c_str());
 	else
 	{
-		struct dirent *dir_info=NULL;
-		while((dir_info=readdir(path)))
+		struct dirent *dir_info = NULL;
+		while ((dir_info = readdir(path)))
 		{
-			if(strcmp(dir_info->d_name,".")==0||strcmp(dir_info->d_name,".."))
+			if (strcmp(dir_info->d_name, ".") == 0 || strcmp(dir_info->d_name, ".."))
 				continue;
-			std::string temp=(src+"/")+dir_info->d_name;
+			std::string temp = (src + "/") + dir_info->d_name;
 			del_file(temp);
 		}
 		remove(src.c_str());
 	}
-	if(closedir(path)==-1)
+	if (closedir(path) == -1)
 	{
 		perror("closedir error");
 		exit(EXIT_FAILURE);
@@ -75,89 +76,100 @@ int del_file(const std::string &src)
 }
 
 // src is a relative path of a file.
-// dir is a relative path of a dir.
-int file_exist(const std::string &src,const std::string &dir)
+// // dir is a relative path of a dir.
+int file_exist(std::string src, std::string dir)
 {
 	std::vector<std::string> src_split_path;
-	size_t prepos=0,nowpos=0;
-	while((nowpos=src.find("/",nowpos))!=std::string::npos)
+	size_t prepos = 0, nowpos = 0;
+	while ((nowpos = src.find("/", nowpos)) != std::string::npos) // split src by '/'
 	{
-		src_split_path.push_back(src.substr(prepos,nowpos-prepos));
-		prepos=++nowpos;
+		src_split_path.push_back(src.substr(prepos, nowpos - prepos));
+		prepos = ++nowpos;
 	}
-	src_split_path.push_back(src.substr(prepos,nowpos-prepos));
-	std::string rela_path=dir+"/";
-	int flag=0;
-	int size=src_split_path.size();
-	for(int i=0;i<size;++i)
+	src_split_path.push_back(src.substr(prepos, nowpos - prepos));
+
+	std::string rela_path = dir + "/"; // relative path of src in dir
+
+	int flag = 0;
+	int size = src_split_path.size();
+	for (int i = 0; i < size; ++i)
 	{
-		if(flag)
-			rela_path=rela_path+src_split_path[i]+"/";
-		if(src_split_path[i]==SRC_DIR.substr(SRC_DIR.rfind("/")+1))
-			flag=1;
+		if (flag)
+			rela_path = rela_path + src_split_path[i] + "/";
+		if (src_split_path[i] == SRC_DIR.substr(SRC_DIR.rfind("/") + 1))
+			flag = 1;
 	}
-	rela_path.pop_back();
-	if(access(rela_path.c_str(),F_OK)==0)
+	rela_path.pop_back(); // pop_back() to pop '/'
+	if (access(rela_path.c_str(), F_OK) == 0)
 		return 1;
 	return 0;
 }
 
-int cp_file(const std::string &src,const std::string &des)
+int cp_file(std::string src, std::string des)
 {
 	umask(0);
-	int fd_src,fd_des;
-	if((fd_src=open(src.c_str(),O_RDONLY))!=0)
-		return perror("src open fail"),1;
-	if((fd_des=open(des.c_str(),O_WRONLY|O_CREAT,0666))!=0)
-		return perror("src open fail"),1;
+	int fd_src, fd_des;
+	if ((fd_src = open(src.c_str(), O_RDONLY)) == -1)
+		return perror("124 src open fail"), 1;
+	if ((fd_des = open(des.c_str(), O_WRONLY | O_CREAT, 0666)) == -1)
+		return perror("126 src open fail"), 1;
 
-	int size=lseek(fd_src,0,SEEK_END);
-	pthread_t thread=0;
-	std::vector<Data_trans*> args;
+	int size = lseek(fd_src, 0, SEEK_END);
+
+	pthread_t thread = 0;
+	std::vector<Data_trans *> args;
 	std::vector<pthread_t> threads;
-	if(size>SIGLE_THREAD_MAX_DATA_SIZE)
+
+	if (size > SIGLE_THREAD_MAX_DATA_SIZE)
 	{
-		for(int i=0;i<size/SIGLE_THREAD_MAX_DATA_SIZE;++i)
+		printf("MULTITHREAD\n");
+		for (int i = 0; i < size / SIGLE_THREAD_MAX_DATA_SIZE; ++i)
 		{
 			threads.push_back(thread);
-			args.push_back((Data_trans*)malloc(sizeof(Data_trans)));
-			args.back()->begin=i*SIGLE_THREAD_MAX_DATA_SIZE;
-			args.back()->size=SIGLE_THREAD_MAX_DATA_SIZE;
-			args.back()->src=src;
-			args.back()->des=des;
-			if(pthread_create(&threads[i],NULL,data_trans_thread,args.back())!=0)
-				return perror("pthread_create error"),1;
+			Data_trans *t = (Data_trans *)malloc(sizeof(Data_trans));
+			t->begin = i * SIGLE_THREAD_MAX_DATA_SIZE;
+			t->size = SIGLE_THREAD_MAX_DATA_SIZE;
+			strcpy(t->src, src.c_str());
+			strcpy(t->des, des.c_str());
+			args.push_back(t);
+			if (pthread_create(&threads[i], NULL, data_trans_thread, args[i]) != 0)
+				return perror("pthread_create error"), 1;
 		}
-		if(size%SIGLE_THREAD_MAX_DATA_SIZE)
+		if (size % SIGLE_THREAD_MAX_DATA_SIZE)
 		{
 			threads.push_back(thread);
-			args.push_back((Data_trans*)malloc(sizeof(Data_trans)));
-			args.back()->begin=size/SIGLE_THREAD_MAX_DATA_SIZE*SIGLE_THREAD_MAX_DATA_SIZE;
-			args.back()->size=size-size/SIGLE_THREAD_MAX_DATA_SIZE*SIGLE_THREAD_MAX_DATA_SIZE;
-			args.back()->src=src;
-			args.back()->des=des;
-			if(pthread_create(&threads[threads.size()-1],NULL,data_trans_thread,args.back())!=0)
-				return perror("pthread_create error"),1;
+			Data_trans *t = (Data_trans *)malloc(sizeof(Data_trans));
+			;
+			t->begin = size / SIGLE_THREAD_MAX_DATA_SIZE * SIGLE_THREAD_MAX_DATA_SIZE;
+			t->size = size - size / SIGLE_THREAD_MAX_DATA_SIZE * SIGLE_THREAD_MAX_DATA_SIZE;
+			strcpy(t->src, src.c_str());
+			strcpy(t->des, des.c_str());
+			args.push_back(t);
+			if (pthread_create(&threads[threads.size() - 1], NULL, data_trans_thread, args[size / SIGLE_THREAD_MAX_DATA_SIZE]) != 0)
+				return perror("pthread_create error"), 1;
 		}
 
-		for(int i=0;i<size/SIGLE_THREAD_MAX_DATA_SIZE;++i)
+		printf("WAIT\n");
+		for (int i = 0; i < size / SIGLE_THREAD_MAX_DATA_SIZE; ++i)
 		{
 			void *re;
-			pthread_join(threads[i],&re);
-			if(*(int*)re!=0)
+			if (pthread_join(threads[i], &re))
+				perror("166 pthread_join error");
+			if (*(int *)re != 0)
 				return 1;
 		}
-		if(size%SIGLE_THREAD_MAX_DATA_SIZE)
+		if (size % SIGLE_THREAD_MAX_DATA_SIZE)
 		{
 			void *re;
-			pthread_join(threads.back(),&re);
-			if(*(int*)re!=0)
+			if (pthread_join(threads[size / SIGLE_THREAD_MAX_DATA_SIZE], &re))
+				perror("175 pthread_join error");
+			if (*(int *)re != 0)
 				return 1;
 		}
 	}
 	else
 	{
-		
+		data_from_to(src, des, 0, size);
 	}
 	return 0;
 }
